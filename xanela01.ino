ef0226e9b2cac174bfa577d8355346a6a8905fb5
pablocameangarrido <152868590+pablocameangarrido@users.noreply.github.com>
2024-02-28 16:49:06 +0100

    xanela01.ino
    https://www.tinkercad.com/things/b4dpNsuXi7k-xanela-de-coche?sharecode=cXscpVufhzV3fDL5pdKHxoIoyP_RwOu2rLAlmQSeN9w

--- xanela01.ino
+++ xanela01.ino
@@ -4,38 +4,54 @@
 *  Autor: Pablo Cameán Garrido
 */
 
-
+//Declaración de pines
 #define PULS 10
 #define MOTORUP 13
 #define MOTORDOWN 12
 
-void setup(){
+//Declaración de variables
+int contador = 0;
+int motorActivo = 0;
+
+void setup() {
   pinMode(PULS, INPUT_PULLUP);
   pinMode(MOTORUP, OUTPUT);
   pinMode(MOTORDOWN, OUTPUT);
+  Serial.begin(9600);
 }
 
-void loop(){
-  
-      while(digitalRead(PULS) == 0){ //Mientras no se detecte pulso los motores estarán apagados
-    	digitalWrite(MOTORDOWN, LOW);
-      	digitalWrite(MOTORUP, LOW);
-      }
-    digitalRead(PULS) == 1;          // Cuando se detecte el primer pulso se encendera el motor de subida, durante 7 segundos, que es lo que tardaría en subir la ventanilla
-    digitalWrite(MOTORUP, HIGH);
-    delay(7000);
-    digitalWrite(MOTORUP, LOW);     // Apágase aos 7 segundos, o que sería que chegou ao tope
-  
-  // Mismo proceso pero co motor de baixada
+void loop() {
+  if (digitalRead(PULS) == 1) {
+    motorActivo = !motorActivo;  // Cambia el estado del motor activo
 
-  while(digitalRead(PULS) == 0){    
-    	digitalWrite(MOTORDOWN, LOW);
-      	digitalWrite(MOTORUP, LOW);
-      }
-  
-    digitalRead(PULS) == 1;
-  	digitalWrite(MOTORDOWN, HIGH);
-    delay(7000);
+    // Apaga ambos motores al cambiar el motor activo
+    digitalWrite(MOTORUP, LOW);
     digitalWrite(MOTORDOWN, LOW);
 
+    if (motorActivo == 1) {
+      digitalWrite(MOTORUP, HIGH);  // Activa el motor ascendente
+      Serial.println("MotorUP activado");
+    } else {
+      digitalWrite(MOTORDOWN, HIGH);  // Activa el motor descendente
+      Serial.println("MotorDOWN activado");
+    }
+
+    contador = 530; // Espera 7 segundos para apagar el motor
+    
+    while(digitalRead(PULS) == 1) { // Espera a que se suelte el pulsador
+      delay(10);
+    }
+  }
+
+  if (contador > 0) {
+    contador -= 1;
+    delay(10);
+  } else {
+    digitalWrite(MOTORUP, LOW);
+    digitalWrite(MOTORDOWN, LOW);
+    Serial.println("Ambos motores apagados");
+  }
+
+  Serial.print("Contador: ");
+  Serial.println(contador);
 }
